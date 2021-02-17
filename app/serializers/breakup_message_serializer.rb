fb517f9dd8fb387b3b734cdd69965fb635efa083
slurio <samantha.lurio@gmail.com>
2021-02-17 16:58:59 -0500

    updated breakupMessage, quiz, topic serializers

--- app/serializers/breakup_message_serializer.rb
+++ app/serializers/breakup_message_serializer.rb
@@ -1,4 +1,4 @@
 class BreakupMessageSerializer < ActiveModel::Serializer
   attributes :id, :message, :tone, :subject
-  has_one :topic
+  
 end
--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,3 +1,11 @@
 class TopicSerializer < ActiveModel::Serializer
-  attributes :id, :theme
+  attributes :id, :theme, :quizzes, :breakup_messages
+  
+  def quizzes
+    ActiveModel::SerializableResource.new(object.quizzes,  each_serializer: QuizSerializer)
+  end
+
+  def breakup_messages
+    ActiveModel::SerializableResource.new(object.breakup_messages,  each_serializer: BreakupMessageSerializer)
+  end
 end
